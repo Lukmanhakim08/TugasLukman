8e32ac7af9ee73d1f237a082788cd6625849a172
Lukman <lukman081217@gmail.com>
2022-03-14 18:02:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugaslukman/GopayActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tugaslukman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GopayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gopay)
+    }
+}
